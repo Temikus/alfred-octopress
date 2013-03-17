0daa06756e8aef4c1926cc90fcc49c050ae4af05
Artem Yakimenko <temikz@gmail.com>
2013-03-17 13:58:30 +0400

    Updated extension to Alfred V2, added generate option

--- post.sh
+++ post.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#Path for your octopress installation
-octopath=/path/to/your/octopress
+
+#Path to your octopress installation
+octopath='/path/to/your/octopress'
 #Editor to open the new markdown post in
 editorapp=Mou
 
@@ -11,5 +12,5 @@ fi
 
 cd $octopath
 rvm use 1.9.3@octopress
-octopost=$(rake new_post["{query}"]| grep -o 'source/_posts/.*')
+octopost=$(rake new_post['{query}']| grep -o 'source/_posts/.*')
 open -a "$editorapp" $octopath/$octopost
