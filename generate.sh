1d684bc32dbde0971440d6b79953486620c01cb6
Artem Yakimenko <temikz@gmail.com>
2013-04-21 17:06:43 +0400

    Fixed encoding for non-standard characters. Debugging option added. Minor fixes.

--- generate.sh
+++ generate.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-#NOTE:
-#Requires terminal-notifier for OS X notifications:
-#gem install terminal-notifier
-
 #Path to your octopress installation
-octopath='/path/to/your/octopress'
+OCTOPATH='/your/path/to/octopress'
 
 if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
       source "$HOME/.rvm/scripts/rvm"
 fi
 
-cd $octopath
+cd $OCTOPATH
 rvm use 1.9.3@octopress
 rake generate
+
 if [[ $? -eq 0 ]] ; then
     terminal-notifier -message "Blog generated successfully!" -title "Octopress - Alfred"
 else
