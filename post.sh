42bc436f7f115819009a81f63c6dc1f43df96711
Artem Yakimenko <code@temik.me>
2015-05-27 20:23:00 +0200

    Added Rbenv support

--- post.sh
+++ post.sh
@@ -3,17 +3,23 @@
 #Path to your octopress installation
 OCTOPATH='/your/path/to/octopress'
 #Editor to open the new markdown post in
-EDITORAPP=Mou
-
-if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-
-      source "$HOME/.rvm/scripts/rvm"
-fi
+EDITORAPP='Mou'
+#Ruby version from RVM or rbenv
+RUBY_VERSION='1.9.3-p551'
+#Rbenv location
+RBENV_BIN='/usr/local/bin/rbenv'
 
 #Exporting encoding
 export 'LC_CTYPE=en_US.UTF-8'
 
 cd $OCTOPATH
-rvm use 1.9.3@octopress
-OCTOPOST=$(rake new_post['{query}']| grep -o 'source/_posts/.*')
+
+#Initialize RVM
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" && rvm use $RUBY_VERSION
+#Initialise rbenv
+if [[ -s "$RBENV_BIN" ]]; then
+  eval "$($RBENV_BIN init -)"
+fi
+
+OCTOPOST=$(rake --trace new_post['{query}']| grep -o 'source/_posts/.*')
 open -a "$EDITORAPP" $OCTOPATH/$OCTOPOST
